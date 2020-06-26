26c2a33d61148da0d85c218c629888e1c99fa0c0
Lucian <Lucian>
2020-06-26 17:30:14 +0300

    added messages view

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import * as AppActions from '../state/app.actions';
 import * as fromApp from '../state/app.reducer';
 import { Store } from '@ngrx/store';
-
+import { Information } from './information';
+import { InformationService } from './information.service';
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
@@ -11,13 +12,20 @@ import { Store } from '@ngrx/store';
 })
 export class InformationComponent implements OnInit {
 
-  constructor(private store: Store<fromApp.State>) { }
+  informations: Information[];
+  constructor(private store: Store<fromApp.State>, private informationService: InformationService,
+    private cdr : ChangeDetectorRef) { }
 
   toggleSidebar(event) {
     this.store.dispatch(AppActions.toggleSidebar({ data: event }));
   }
 
   ngOnInit(): void {
+    //Emp1 should be replaced with the username of the logged user
+    this.informationService.findAll('Emp1').subscribe(data => {
+      this.informations = data;
+    });
+    this.cdr.detectChanges();
   }
 
 }
